2988ea134355bfeb80a7d6b22d94f49fa1059a5d
VadimBashovyi <81907390+VadimBashovyi@users.noreply.github.com>
2022-07-25 00:58:24 +0000

    Deploying to gh-pages from @ VadimBashovyi/phenomenon-project@e7c7037995456bdef1a1008dfe7a46ade30240c1 🚀

---
+++ index.5905348b.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu-button-close]"),n=document.querySelector("[data-menu]"),o=document.querySelector("[data-modal]");console.log(o);var c=function(){n.classList.toggle("is-open"),o.classList.toggle("is-hidden")};e.addEventListener("click",c),t.addEventListener("click",c)}();
+//# sourceMappingURL=index.5905348b.js.map
